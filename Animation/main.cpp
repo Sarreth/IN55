a7e203ec2e29e5d6bf60983214c463ccf4ff076a
Sarreth <kevin.w.tomas@gmail.com>
2014-04-29 22:16:07 +0200

    Splash Screen
    Mise en place du Splash Screen

--- Animation/main.cpp
+++ Animation/main.cpp
@@ -1,10 +1,20 @@
 #include "MainWindow.h"
+#include <QSplashScreen>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QPixmap pixmap("pics/splash.jpg");
+    QSplashScreen splash(pixmap);
+
+    splash.show();
+    splash.showMessage("<font color=white size=30 stretch=100>Loading, please wait...");
+    qApp->processEvents();
+
     MainWindow w;
     w.showFullScreen();
+    splash.finish(&w);
 
     return a.exec();
 }
